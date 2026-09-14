5d731314a85597982363a2fe67148827d3d9db98
наталья конева <koneva77@yandex.ru>
2026-09-14 17:35:03 +0500

    Creating a controller and a template for displaying a page with detailed product information.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home_view, contacts_view
+from catalog.views import home_view, contacts_view, products_list, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home_view, name='home_view'),
     path('contacts/', contacts_view, name='contacts_view'),
+    path('products_list/', products_list, name='products_list'),
+    path('product_detail/<int:product_id>', product_detail, name='product_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,7 @@
+from itertools import product
 from django.shortcuts import render
+from django.utils.translation.trans_real import catalog
+from .models import Product
 
 
 def home_view(request):
@@ -7,3 +10,19 @@ def home_view(request):
 
 def contacts_view(request):
     return render(request, 'contacts.html')
+
+
+def index(request):
+    return render(request, 'products_list.html')
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products_list.html', context)
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
